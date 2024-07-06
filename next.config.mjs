98faf2184729347453ed574648c0d52e0bf83182
adex-hub <adeolaweb@gmail.com>
2024-07-06 23:12:13 +0100

    update: Made changes to "next.config.mjs" file

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  publicRuntimeConfig: {
-    SERVICE_ID: process.env.SERVICE_ID,
-    TEMPLATE_ID: process.env.TEMPLATE_ID,
-    PUBLIC_KEY: process.env.PUBLIC_KEY,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
