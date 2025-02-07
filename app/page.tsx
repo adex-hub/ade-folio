4574228fce0294bc86ed6c6d044b2523bdbd41a6
adex-hub <adeolaweb@gmail.com>
2025-02-07 19:33:48 +0100

    fix: overflow issue

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const blobity = useBlobity(initialBlobityOptions);
 
   return (
-    <main className="overflow-x-hidden">
+    <main>
       <Hero />
       <Works />
       <About />
