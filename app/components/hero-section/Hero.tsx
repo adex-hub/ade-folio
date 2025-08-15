c6ecac6559c3c877da266251d1ec4c3e2509f870
adex-hub <adeolaweb@gmail.com>
2025-08-15 14:51:44 +0100

    feat: enhance Hero section with ContraButton and update text for clarity

--- app/components/hero-section/Hero.tsx
+++ app/components/hero-section/Hero.tsx
@@ -13,6 +13,7 @@ import {
 import "intersection-observer";
 import { useInView } from "react-intersection-observer";
 import { useView } from "@/contexts/ViewContext";
+import ContraButton from "../about-section/ContraButton";
 
 export default function Hero() {
   const handWaveAnimation = {
@@ -62,58 +63,67 @@ export default function Hero() {
   return (
     <section
       ref={ref}
-      className="pt-36 sm:pt-0 flex flex-col sm:flex-row h-dvh items-center gap-6 sm:justify-between"
+      className="pt-36 lg:pt-0 flex flex-col lg:flex-row h-dvh items-center lg:justify-between mb-12 lg:mb-0"
       id="home"
     >
-      <div className="text sm:w-[60%]">
-        <motion.div
-          className="grid grid-cols-9 w-fit smm:flex gap-2 mb-2 xl:mb-6"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.1, ease: "easeOut" }}
-        >
-          <p className="text-white/60 text-xl smm:text-2xl mb-3 smm:mb-0 lg:text-3xl col-span-6">
-            Hey, there
-          </p>
+      <div className="text sm:w-4/5 text-center lg:text-start lg:w-[60%] flex flex-col gap-6 lg:gap-11 mb-6 lg:mb-0">
+        <div className="flex flex-col gap-4 lg:gap-0">
           <motion.div
-            animate={handWaveAnimation}
-            style={{ transformOrigin: "bottom right" }}
-            className="col-span-3"
+            className="grid grid-cols-9 w-fit smm:flex gap-2 mx-auto lg:mx-0"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.1, ease: "easeOut" }}
           >
-            <Image
-              src="/hand-wave.svg"
-              width={30}
-              height={30}
-              alt="hand-waving"
-            />
+            <p className="text-white/60 text-xl smm:text-2xl lg:text-3xl col-span-6">
+              Hey, there
+            </p>
+            <motion.div
+              animate={handWaveAnimation}
+              style={{ transformOrigin: "bottom right" }}
+              className="col-span-3"
+            >
+              <Image
+                src="/hand-wave.svg"
+                width={30}
+                height={30}
+                alt="hand-waving"
+              />
+            </motion.div>
           </motion.div>
-        </motion.div>
-        <motion.h1
-          className="text-[32px] smm:text-[40px] md:text-5xl lg:text-6xl xl:text-7xl leading-tight font-bold"
-          initial={{ opacity: 0 }}
-          animate={animateIn1}
-        >
-          <p className="text-white/60 inline">I&apos;m </p>
-          <span className="bg-linear-to-br bg-clip-text text-transparent from-[#7CC0C4] via-[#548FBA] to-[#3C84C7]">
-            Adeola Badero
-          </span>
-          <p>a Software Engineer</p>
-        </motion.h1>
+          <motion.h1
+            className="text-[32px] smm:text-[40px] md:text-5xl lg:text-6xl xl:text-7xl leading-tight font-bold"
+            initial={{ opacity: 0 }}
+            animate={animateIn1}
+          >
+            <p className="text-white/60 inline">I&apos;m </p>
+            <span className="bg-linear-to-br bg-clip-text text-transparent from-[#7CC0C4] via-[#548FBA] to-[#3C84C7]">
+              Adeola Badero
+            </span>
+            <p>a Design Engineer</p>
+          </motion.h1>
 
-        <motion.p
-          initial={{ opacity: 0 }}
-          animate={animateIn2}
-          className="text-white/40  text-xl smm:text-2xl lg:text-3xl xl:text-4xl mt-3 smm:mt-6 "
-        >
-          currently focused on building user experiences that drive growth.
-        </motion.p>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={animateIn2}
+            className="text-white/40  text-xl smm:text-2xl lg:text-3xl my-3"
+          >
+            who works across the stack to deliver websites and web apps that
+            drive business growth forward.
+          </motion.p>
+        </div>
+
+        <div className="w-fit mx-auto lg:mx-0">
+          <ContraButton />
+        </div>
       </div>
 
       {/* IMAGE */}
-      <div data-blobity-tooltip="Soft man">
+      <div data-blobity-tooltip="Engineer">
         <motion.div
           ref={imgRef}
-          style={{ rotate }}
+          style={{
+            rotate: window.innerWidth >= 1024 ? rotate : "0deg",
+          }}
           className="h-image flex items-center w-[310px] h-[380px] xl:w-[390px] xl:h-[470px] justify-center relative"
           initial={{ opacity: 0 }}
           animate={animateIn1}
