7165c0b82b00e94099df65e75f2f01be6a471e26
Liliana <constantinolilianam@gmail.com>
2025-09-30 13:45:51 -0700

    updated profile picture and added text under name

--- app/components/hero-section/Hero.tsx
+++ app/components/hero-section/Hero.tsx
@@ -1,53 +1,12 @@
 "use client";
 import Image from "next/image";
-import React, { useEffect, useRef } from "react";
-import {
-  delay,
-  easeIn,
-  easeInOut,
-  motion,
-  useScroll,
-  useTransform,
-} from "framer-motion";
-// @ts-ignore
-import "intersection-observer";
+import React, { useEffect } from "react";
+import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import { useView } from "@/contexts/ViewContext";
 
 export default function Hero() {
-  const handWaveAnimation = {
-    rotate: [0, 15, -10, 15, -10, 15, -10, 15, -10, 15, 0],
-    transition: {
-      duration: 1.5,
-      ease: easeInOut,
-    },
-  };
-
-  const animateIn1 = {
-    opacity: [0, 1],
-    y: ["1rem", "0px"],
-    transition: {
-      delay: 1.5,
-      duration: 0.7,
-      ease: easeIn,
-    },
-  };
-
-  const animateIn2 = {
-    ...animateIn1,
-    transition: {
-      ...animateIn1.transition,
-      delay: 2,
-    },
-  };
-
   const { setSectionInView } = useView();
-
-  const imgRef = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: imgRef,
-  });
-
   const { ref, inView } = useInView({
     threshold: 0.4,
     rootMargin: "-100px 0px",
@@ -57,61 +16,50 @@ export default function Hero() {
     if (inView) setSectionInView("home");
   }, [inView, setSectionInView]);
 
-  const rotate = useTransform(scrollYProgress, [0, 1], ["0deg", "-15deg"]);
-
   return (
     <section
       ref={ref}
       className="pt-36 sm:pt-0 flex flex-col sm:flex-row h-dvh items-center gap-6 sm:justify-between"
       id="home"
     >
+      {/* Text */}
       <div className="text sm:w-[60%]">
-        <motion.div
-          className="grid grid-cols-9 w-fit smm:flex gap-2 mb-2 xl:mb-6"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.1, ease: "easeOut" }}
-        >
-          {/* <p className="text-white/60 text-xl smm:text-2xl mb-3 smm:mb-0 lg:text-3xl col-span-6">
-            Hey, there
-          </p> */}
-          <motion.div
-            // animate={handWaveAnimation}
-            // style={{ transformOrigin: "bottom right" }}
-            // className="col-span-3"
-          >
-            {/* <Image
-              src="/hand-wave.svg"
-              width={30}
-              height={30}
-              alt="hand-waving"
-            /> */}
-          </motion.div>
-        </motion.div>
         <motion.h1
           className="text-[32px] smm:text-[40px] md:text-5xl lg:text-6xl xl:text-7xl leading-tight font-bold"
-          initial={{ opacity: 0 }}
-          animate={animateIn1}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7, ease: "easeOut" }}
         >
-          {/* <p className="text-white/60 inline">I&apos;m </p> */}
           <span className="bg-linear-to-br bg-clip-text text-transparent from-[#FFB3F2] via-[#FFB3F2] to-[#FFB3F2]">
             Liliana Constantino
           </span>
         </motion.h1>
 
         <motion.p
-          initial={{ opacity: 0 }}
-          animate={animateIn2}
-          className="text-white/40  text-xl smm:text-2xl lg:text-3xl xl:text-4xl mt-3 smm:mt-6 "
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.3, duration: 0.7, ease: "easeOut" }}
+          className="text-white/60 text-xl smm:text-2xl lg:text-3xl xl:text-4xl mt-4"
         >
-          {/* currently focused on building user experiences that drive growth. */}
+          Currently focused on elevating my skills in front-end and back-end development through hand on projects as well as learning about AI foundations and Generative AI through Oracle Univeristy Certification.
         </motion.p>
       </div>
 
-      {/* IMAGE */}
-      <div data-blobity-tooltip="Soft man">
-     
-      </div>
+      {/* Image */}
+      <motion.div
+        className="flex items-center w-[310px] h-[380px] xl:w-[390px] xl:h-[470px] justify-center relative"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.6, duration: 0.7, ease: "easeOut" }}
+      >
+        <Image
+          src="/barcelona.jpg" 
+          priority
+          fill
+          alt="Liliana's profile picture"
+          className="object-cover rounded-xl shadow-lg"
+        />
+      </motion.div>
     </section>
   );
 }
